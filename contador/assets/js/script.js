e05c97f1502aaf0f49cb7dbce20a154fc3497e81
Rodrigo Rocha <61064085+rodi38@users.noreply.github.com>
2022-07-05 12:37:22 -0300

    Update script.js

--- contador/assets/js/script.js
+++ contador/assets/js/script.js
@@ -8,14 +8,12 @@ function incrementar() {
     dado.innerHTML = contador++
     contador > 20? contador = 20 : false
     contador > 10? dado.style.color = "#57f129" : false
-    console.log(contador)
 }
 
 function decrementar() {
     dado.innerText = contador--
     contador < 0? contador = 0 : false
     contador < 10? dado.style.color = "white" : false
-    console.log(contador)
 }
 
 incremento.addEventListener("click", incrementar);
